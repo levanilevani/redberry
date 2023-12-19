8863643c272d7f307a8ab7f9a14d645add753051
levanilevani <levanilevanikilasonia@gmail.com>
2023-12-19 23:12:58 +0400

    added blog card tags

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,26 @@
 import {BlogCard} from "../../components/common/BlogCard/BlogCard.jsx";
 
 export const Home = () => {
-  return <BlogCard/>
+  return (
+    <BlogCard
+      id={1} title={'Blog title'}
+      description={'Blog description'}
+      imgSrc={'https://via.placeholder.com/150'} publishDate={'2023-11-19 00:00:00'}
+      categories={[
+        {
+          "id": 1,
+          "name": "Category",
+          "text_color": "#ffffff",
+          "background_color": "#000000"
+        },
+        {
+          "id": 2,
+          "name": "Category",
+          "text_color": "red",
+          "background_color": "rgba(250, 87, 87, 0.08)"
+        }
+
+      ]}
+      author={'გელა გელაშვილი'}
+    />)
 };
