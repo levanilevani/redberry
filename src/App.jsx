bbfb5bc8496514cc728018126497607b46671e70
levanilevani <levanilevanikilasonia@gmail.com>
2023-12-20 02:10:26 +0400

    blog page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { MainLayout, SeccondaryLayout } from './layouts';
-import { Home } from './pages';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {MainLayout, SeccondaryLayout} from './layouts';
+import {Home, Blog} from './pages';
 
 import './assets/styles/global.scss';
 
@@ -8,12 +8,13 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<MainLayout />}>
-          <Route path='/' element={<Home />} />
+        <Route element={<MainLayout/>}>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/blog/:id' element={<Blog/>}/>
         </Route>
 
-        <Route element={<SeccondaryLayout />}>
-          <Route path='/secondary' element={<di>test secondary layout</di>} />
+        <Route element={<SeccondaryLayout/>}>
+          <Route path='/secondary' element={<di>test secondary layout</di>}/>
         </Route>
       </Routes>
     </BrowserRouter>
